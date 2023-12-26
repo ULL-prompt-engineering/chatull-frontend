24d2de9780ed3de1f35ae0296a7425652d6a1ee1
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2023-12-26 21:29:14 +0000

    Cambio de Repositorio

---
+++ src/js/answer_request.js
@@ -0,0 +1,62 @@
+import { loadQuestion } from './load_questions.js';
+
+function obtenerRespuesta(answer, input, selector, button) {
+  button.disabled = true;
+  button.innerHTML = "Obteniendo respuesta...";
+  let question = input.value;
+  input.value = "";
+  // adapta question para que sea compatible con la URL
+  question = question.replace(/ /g, "%20");
+  let url = "";
+  if (selector.value != "Reglamentación y Normativa") {
+      url = 'http://127.0.0.1:5000/get_answer?question=' + question + '&subject=' + selector.value;
+  } else {
+      url = 'http://127.0.0.1:5000/get_teacher_answer?question=' + question;
+  }
+  console.log(url);
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      answer.innerHTML = data.answer;
+      let questionObject = {
+            "question": question.replace(/%20/g, " "),
+            "answer": data.answer
+        };
+      if (!localStorage.getItem(selector.value) || localStorage.getItem(selector.value) == "null") {
+            localStorage.setItem(selector.value, JSON.stringify([questionObject]));
+      } else {
+            let questions = JSON.parse(localStorage.getItem(selector.value));
+            questions.push(questionObject);
+            localStorage.setItem(selector.value, JSON.stringify(questions));
+      }
+        loadQuestion(questionObject);
+        input.scrollIntoView();
+        button.disabled = false;
+        button.innerHTML = "Enviar";
+  })
+  .catch(error => {
+      console.error('Error:', error);
+      answer.innerHTML = "Error al obtener la respuesta.";
+  });
+}
+
+// Encuentra todos los botones con la clase `alert` en la página.
+const button = document.querySelector('button.question');
+const input = document.querySelector('input.input-question');
+const answer = document.querySelector('div.answer');
+const selector = document.querySelector('select.subject');
+// Maneja los clics en cada botón.
+
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        button.click();
+    }
+});
+
+button.addEventListener('click', async() => {
+    obtenerRespuesta(answer, input, selector, button);
+});
+
+
---
+++ src/js/load_questions.js
@@ -0,0 +1,69 @@
+/*
+<div class="message-container user-message flex justify-end">
+    <div class="UserMessage bg-blue-500 text-white rounded-lg p-2 mb-5 font-bold">
+        Usuario
+        <div class="message bg-green-500 text-white rounded-lg p-2 mt-2">
+            ¿Cuál es tu nombre?
+        </div>
+    </div>
+</div>
+<div class="message-container bot-message flex">
+    <div class="BotMessage bg-purple-600 rounded-lg p-2 mb-5 font-bold">
+        ChatUll
+        <div class="message bg-gray-300 text-gray-700 rounded-lg p-2 mt-2">
+            Hola, soy ChatUll. ¿En qué puedo ayudarte?
+        </div>
+    </div>
+</div>
+*/
+
+function loadQuestion(questionData) {
+    if (questionData.question && questionData.answer) {
+        let messageContainer = document.createElement("div");
+        messageContainer.setAttribute("class", "message-container user-message flex justify-end");
+        let userMessage = document.createElement("div");
+        userMessage.setAttribute("class", "UserMessage bg-blue-500 text-white rounded-lg p-2 mb-5 font-bold");
+        userMessage.innerHTML = "Usuario";
+        let message = document.createElement("div");
+        message.setAttribute("class", "message bg-white text-black rounded-lg p-2 mt-2");
+        message.innerHTML = questionData.question;
+        userMessage.appendChild(message);
+        messageContainer.appendChild(userMessage);
+        document.querySelector("div.chat-container").appendChild(messageContainer);
+        messageContainer = document.createElement("div");
+        messageContainer.setAttribute("class", "message-container bot-message flex");
+        let botMessage = document.createElement("div");
+        botMessage.setAttribute("class", "BotMessage bg-purple-600 rounded-lg p-2 mb-5 font-bold");
+        botMessage.innerHTML = "ChatUll";
+        message = document.createElement("div");
+        message.setAttribute("class", "message bg-gray-300 text-gray-700 rounded-lg p-2 mt-2");
+        message.innerHTML = questionData.answer;
+        botMessage.appendChild(message);
+        messageContainer.appendChild(botMessage);
+        document.querySelector("div.chat-container").appendChild(messageContainer);
+        messageContainer.scrollIntoView();
+    }
+}
+const selector = document.querySelector('select.subject');
+let questions = localStorage.getItem(selector.value);
+
+if (questions) {
+    questions = JSON.parse(questions);
+    questions.forEach((question) => {
+        loadQuestion(question);
+    });
+}
+
+selector.addEventListener("change", function(event) {
+    console.log("change");
+    let questions = localStorage.getItem(selector.value);
+    document.querySelector("div.chat-container").innerHTML = "";
+    if (questions) {
+        questions = JSON.parse(questions);
+        questions.forEach((question) => {
+            loadQuestion(question);
+        });
+    }
+});
+
+export { loadQuestion };
