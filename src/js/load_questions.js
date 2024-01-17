2a613149dccf77b8529e1c456f5f78ea42f51103
Jonay Vera <alu0101398198@ull.edu.es>
2024-01-17 20:20:13 +0000

    Interface Update

--- src/js/load_questions.js
+++ src/js/load_questions.js
@@ -20,12 +20,15 @@
 function loadQuestion(questionData, isQuestion) {
     if (isQuestion) {
         let messageContainer = document.createElement("div");
-        messageContainer.setAttribute("class", "message-container user-message flex justify-end");
+        messageContainer.setAttribute("class", "message-container bg-[#a3bcff] user-message flex flex-row items-center mt-5 rounded-lg");
         let userMessage = document.createElement("div");
-        userMessage.setAttribute("class", "UserMessage bg-blue-500 text-white rounded-lg p-2 mb-5 font-bold");
-        userMessage.innerHTML = "Usuario";
+        userMessage.setAttribute("class", "UserMessage text-white rounded-lg p-2 mb-5 font-bold");
+        let userPhoto = document.createElement("img");
+        userPhoto.setAttribute("class", "rounded-full ml-4 mr-4 mt-4 mb-4 max-h-16 m-w-16");
+        userPhoto.setAttribute("src", "https://www.clipartmax.com/png/middle/434-4349876_profile-icon-vector-png.png");
+        messageContainer.appendChild(userPhoto);
         let message = document.createElement("div");
-        message.setAttribute("class", "message bg-white text-black rounded-lg p-2 mt-2");
+        message.setAttribute("class", "message text-white rounded-lg");
         message.innerHTML = questionData.question;
         userMessage.appendChild(message);
         messageContainer.appendChild(userMessage);
@@ -33,12 +36,15 @@ function loadQuestion(questionData, isQuestion) {
         messageContainer.scrollIntoView();
     } else {
         let messageContainer = document.createElement("div");
-        messageContainer.setAttribute("class", "message-container bot-message flex");
+        messageContainer.setAttribute("class", "message-container bot-message bg-[#447058] flex flex-row items-center mt-5 rounded-lg");
         let botMessage = document.createElement("div");
-        botMessage.setAttribute("class", "BotMessage bg-purple-600 rounded-lg p-2 mb-5 font-bold");
-        botMessage.innerHTML = "ChatUll";
+        botMessage.setAttribute("class", "BotMessage rounded-lg p-2 mb-5 font-bold");
+        let botPhoto = document.createElement("img");
+        botPhoto.setAttribute("class", "rounded-full ml-4 mr-4 mt-4 mb-4 max-h-16 m-w-16");
+        botPhoto.setAttribute("src", "https://www.ibiae.com/wp-content/uploads/2021/03/SARA-ASISTENTE-VIRTUAL.jpg");
+        messageContainer.appendChild(botPhoto);
         let message = document.createElement("div");
-        message.setAttribute("class", "message bg-gray-300 text-gray-700 rounded-lg p-2 mt-2");
+        message.setAttribute("class", "message text-white rounded-lg");
         message.innerHTML = questionData.answer;
         botMessage.appendChild(message);
         messageContainer.appendChild(botMessage);
