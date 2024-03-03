272e02128c78c24d1db2f5f84c5bb39a9780dfdc
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2024-03-03 12:49:55 +0000

    Api-Key check and minor fixes

--- src/js/session_token.js
+++ src/js/session_token.js
@@ -2,6 +2,14 @@ async function setApiKey() {
     // Capturar la API key ingresada por el usuario
     let apiKey = document.querySelector('input.api-key');
 
+    // Validar que la API key tenga el formato correcto "sk-[48 letras o numeros en minúsculas o mayúsculas]"
+    let apiKeyRegex = /^sk-[a-zA-Z0-9]{48}$/;
+    if (!apiKeyRegex.test(apiKey.value)) {
+        alert('La API key ingresada no es una API key válida');
+        apiKey.value = '';
+        return;
+    }
+
     // Enviar la API key y el token de sesión al servidor (puedes usar fetch u otras opciones)
     await fetch('https://chatull.onrender.com/set_api_key', {
         method: 'POST',
@@ -27,3 +35,7 @@ async function setApiKey() {
 }
 
 document.querySelector('button.send-api-key').addEventListener('click', setApiKey);
+
+document.querySelector('span.api-link').addEventListener('click', () => {
+    window.location.href = 'https://platform.openai.com/account/api-keys';
+});
